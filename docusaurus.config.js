92d2cdd58ff7f76eb786ae0e4b8ffc96136653e0
Maxwell Schmitt <50837674+schmitty2535@users.noreply.github.com>
2023-01-03 20:46:07 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://test.maxschmitty.com/',
+  url: 'http://test.maxschmitty.com/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
