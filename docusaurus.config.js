f96d64d2e3303b722a80d07e549195becbfdbd93
Maxwell Schmitt <m_schmitt214@outlook.com>
2023-02-28 00:45:13 -0500

    Get working

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,16 +33,14 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          //sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath:'/', // Serve the docs at the site's root
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
-          
-          //showReadingTime: true,
+          showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
