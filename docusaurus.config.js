6d02f502d5a59d212378df0a3b5333e122d4e395
schmitty2535 <m_schmitt214@outlook.com>
2023-04-10 08:56:44 -0400

    update favicon to favicon-light-full.ico

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon-light.ico',
+  favicon: 'img/favicon-light-full.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
