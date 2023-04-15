b0d31ddf533fa1351b5f1e3a7001f787b0b6882f
schmitty2535 <m_schmitt214@outlook.com>
2023-04-15 10:51:56 -0400

    added pwa support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -147,6 +147,38 @@ const config = {
         indexName: 'dev_mschmitty'
       }
     }),
+
+  plugins: [
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+        ],
+      },
+    ],
+  ],
+
 };
 
 module.exports = config;
